aac7b1e98daa442b30c517d45b26e0cc02b10f0b
inthependiente <arauco@gmail.com>
2026-06-15 15:37:25 -0400

    devolviendo archivos tailwind y postcss

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          950: '#030712',
+          950: '#030712',
+          850: '#1a1f35',
+          450: '#94a3b8',
+        },
+        indigo: {
+          450: '#a5b4fc',
+          950: '#1f1a4a',
+        },
+      }
+    },
+  },
+  plugins: [],
+}
